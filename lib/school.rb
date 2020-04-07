819e037f0928d675060af3586070b6781e4c281f
Haley Rodzos <haley.rodzos@gmail.com>
2020-04-07 13:12:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+  
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+  
+  def grade(student)
+    roster[student]
+  end
+  
+  def sort
+    sort_rost = Hash.new
+    
+    roster.each do |grade, student|
+      sort_rost[grade] = student.sort
+    end
+    sort_rost
+  end
+end
+
+School.new("Test school")
+
+
